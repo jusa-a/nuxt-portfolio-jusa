eb256e5d805e670acb0dc674e1bc7913c15433a5
jusa-a <jusa.annevirta@aalto.fi>
2023-01-30 11:45:56 +0200

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,17 @@ export default defineNuxtConfig({
                     content: 'Portfolio Jusa'
                 }
             ],
-            link: [{ rel: 'icon', href: '#' }],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                {
+                    rel: 'apple-touch-icon',
+                    sizes: '180x180',
+                    href: '/apple-touch-icon.png'
+                },
+                { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'manifest', href: '/site.webmanifest' }
+            ],
             htmlAttrs: {
                 lang: 'en'
             }
