b28e2e621c45b70a21e4f2178b189d7b59d5b232
jusa-a <jusa.annevirta@aalto.fi>
2023-10-29 21:02:30 +0200

    add link to default config

--- app/router.options.ts
+++ app/router.options.ts
@@ -1,5 +1,7 @@
 import type { RouterConfig } from '@nuxt/schema'
 
+// default config: https://github.com/nuxt/framework/blob/main/packages/nuxt/src/pages/runtime/router.options.ts
+
 // https://router.vuejs.org/api/interfaces/routeroptions.html
 export default <RouterConfig>{
     scrollBehavior(to, from, savedPosition) {
